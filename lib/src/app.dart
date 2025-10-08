f7b4a74d0ab2f311f62cdf30f89bef10df264f28
Mohamed50 <Mohamedalameen50@gmail.com>
2025-10-08 16:40:50 +0400

    refactor imports
    refactor(app): organize imports
    refactor: organize imports in app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import '/src/core/core.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:get/get.dart';
-import '/src/config/config.dart';
+import 'config/config.dart';
+import 'core/core.dart';
 import 'modules/locale/data/services/localization_service.dart';
 import 'modules/connections/connection.dart';
 import 'modules/theme/theme.dart';
